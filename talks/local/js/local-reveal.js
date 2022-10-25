1761fea762b9e1ab8d9450b6c852ea00f0cb7058
Mark Wallace <markianwallace@gmail.com>
2022-10-25 13:22:52 +0100

    Added talks folder

---
+++ talks/local/js/local-reveal.js
@@ -0,0 +1,20 @@
+Reveal.initialize({
+  plugins: [ ],
+  //disableLayout: true,
+  width: 1280,
+  height: 720,
+  margin: 0.04,
+  controls: false,
+  progress: true,
+  autoPlayMedia: true,
+  viewDistance: 20,
+  mobileViewDistance: 5,
+  hash: true,
+  // Hides the address bar on mobile devices
+  hideAddressBar: true,
+  // Transition style
+  transition: "fade", // none/fade/slide/convex/concave/zoom
+  // Transition speed
+  transitionSpeed: "slow", // default/fast/slow,
+  preloadIframes: true
+});
